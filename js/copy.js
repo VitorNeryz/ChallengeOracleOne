fb79a48436614f0b1005c1e9e11745b8fa56822a
Vitor Nery <vitormanoel2020@gmail.com>
2022-02-14 18:57:07 -0300

    adicionado o botão de copiar funcionando e copy.js adicionado

---
+++ js/copy.js
@@ -0,0 +1,8 @@
+var botaoCopiar = document.getElementById("btn-copy");
+var inputFinal = document.getElementById("msg-resultado");
+
+botaoCopiar.addEventListener("click", function(){
+	inputFinal.select();
+	document.execCommand('copy');
+	alert("Copiado para área de transferência");
+})
